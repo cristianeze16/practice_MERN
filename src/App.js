dbb79d58bd406c2829aa55374521dc0d9074ceac
cristianeze16 <cristian.ezequiel.hermosa@gmail.com>
2022-11-01 09:11:41 -0300

    tarea-1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import NombreFoto from "./components/NombreFoto";
+import CampoConArray from "./components/CampoConArray";
+import Flechita from "./components/Flechita";
+import data from "./data/datos";
+import { useState } from "react";
 function App() {
+  console.log(data[0].image)
+  let [mostrarOcultar, setMostrarOcultar] = useState(false);
+  let [numeroAcambiar, setNumeroAcambiar] = useState(0);
+
+  let hide = () => {
+    setMostrarOcultar(!mostrarOcultar);
+    // console.log(mostrarOcultar);
+  };
+
+  let next = () => {
+    if (numeroAcambiar < data.length - 1) {
+      setNumeroAcambiar(++numeroAcambiar);
+    } else {
+      setNumeroAcambiar(0);
+    }
+  };
+  let prev = () => {
+    if (numeroAcambiar > 0) {
+      setNumeroAcambiar(--numeroAcambiar);
+    } else {
+      setNumeroAcambiar(data.length - 1);
+    }
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <h1>Cristian</h1>
+    <div className="card black">
+      <NombreFoto
+        nombre={data[numeroAcambiar].category}
+        foto={data[numeroAcambiar].image}
+      />
+      {mostrarOcultar ? (
+        <>
+          <p className="gray flex j-center a-center" onClick={hide}>
+            ocultar info
+          </p>
+
+          <div className=" flex j-center a-center">
+            <p className="j-center a-center">
+              {data[numeroAcambiar].name}
+            </p>
+            <p className=" white ">
+              description:{data[numeroAcambiar].description}
+            </p>
+          </div>
+        </>
+      ) : (
+        <p className="gray flex j-center a-center" onClick={hide}>
+          Mostrar info
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      )}
+
+      <div className=" flex j-center a-center d-row">
+        <Flechita verbo="Anterior" estilo="r25" click={prev} />
+        <Flechita verbo="Siguiente" estilo="l25" click={next} />
+      </div>
     </div>
   );
 }
---
+++ src/components/Flechita.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Flechita(props) {
+  console.log(props);
+
+  let { verbo, estilo, click } = props; //saco la propidad verbo del objeto props
+
+  return (
+    <div
+      className={`gray flecha  ${estilo}flex j-center a-center`}
+      onClick={click}
+    >
+      {verbo}
+    </div>
+  );
+}
